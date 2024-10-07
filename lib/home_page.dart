b85abeab1c8783754b5a04c41bca83f747376f86
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-08 00:00:00 +0300

    added FeatureBox

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: MyColor.firstSuggestionBoxColor,
+        child: const Icon(Icons.mic),
+      ),
       appBar: AppBar(
         title: const Text('Voice Assistant'),
         leading: const Icon(Icons.menu),
@@ -35,7 +40,86 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          const Column(
+            children: [
+              FeatureBox(
+                color: MyColor.firstSuggestionBoxColor,
+                headerText: 'ChatGPT',
+                description:
+                    'A smarter way too stay organized and informed with ChatGPT',
+              ),
+              FeatureBox(
+                color: MyColor.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                description:
+                    'Get inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              FeatureBox(
+                color: MyColor.thirdSuggestionBoxColor,
+                headerText: 'Smart Voice Assistant',
+                description:
+                    'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              ),
+            ],
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class FeatureBox extends StatelessWidget {
+  final Color color;
+  final String headerText;
+  final String description;
+  const FeatureBox({
+    super.key,
+    required this.color,
+    required this.headerText,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0).copyWith(
+          left: 15,
+        ),
+        child: Column(
+          children: [
+            Text(
+              headerText,
+              style: const TextStyle(
+                fontFamily: 'Cera Pro',
+                color: MyColor.blackColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 3),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Text(
+                description,
+                style: const TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: MyColor.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
