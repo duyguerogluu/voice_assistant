bc2dfecf4f2fe8804f33d242ce90c7125944b93b
duyguerogluu <erogluduyguduygu@gmail.com>
2024-10-10 21:52:07 +0300

    added license

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of voice_assistant.
+ *
+ *  voice_assistant is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  voice_assistant is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *   along with voice_assistant.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import 'package:flutter/material.dart';
 import 'package:voice_assistant/home_page.dart';
 import 'package:voice_assistant/mycolor.dart';
